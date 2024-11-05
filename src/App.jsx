e5c442d93e1283ac6c9beeb20746622232245bff
Amine <amineouzane10@gmail.com>
2024-11-06 00:32:15 +0100

    clean the images and no using code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from './Component/navbar'; // Make sure the path is correct
 import Banner from './Component/Banner';
-import Services from './Component/Services'; // Import the Services component
 import Footer from './Component/footer';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap
 import './App.css';
 import Features from './Component/Features';
-import Contact from './Component/Contact';
 import WhatsAppButton from './Component/WhatsAppButton';
 
 function App() {
@@ -14,9 +12,7 @@ function App() {
     <>
       <Navbar />
       <Banner />
-       {/*<Services />*/}
       <Features />
-      {/*<Contact/>*/}
       <WhatsAppButton />
       <Footer />
 
