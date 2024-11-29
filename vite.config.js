4a6772d8112bb9624e5a5020338abd2683d59973
Amine <amineouzane10@gmail.com>
2024-11-29 17:22:03 +0100

    maj vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    legalComments: 'none',
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          // Ignore "use client" warnings
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+});
